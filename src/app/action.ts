bc8411668940713a1cfc208e01638765084d1dc3
Rinards Sondors <rinards.sondors@gmail.com>
2024-01-14 21:33:45 +0000

    Update dependencies and load more anime cards

--- src/app/action.ts
+++ src/app/action.ts
@@ -2,7 +2,7 @@
 
 export async function fetchAnime(page: number) {
 	const res = await fetch(
-		`https://shikimori.one/api/animes?page=${page}&limit=6&order=popularity`,
+		`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`,
 	);
 	const data = await res.json();
 
