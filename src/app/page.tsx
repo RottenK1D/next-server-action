d5226a3e48a9e4de6e5fe1a0258cb8294976fd77
Rinards Sondors <rinards.sondors@gmail.com>
2024-01-14 20:57:11 +0000

    Update layout and add anime card section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-import Image from "next/image";
+import { AnimeCard, AnimeProp } from "@/components/AnimeCard";
+import { data } from "@/db/data";
 
 export default function Home() {
-	return <></>;
+	return (
+		<main className="s:py-16 py-16 px-8 flex flex-col gap-10">
+			<h2 className="text-3xl text-white font-bold">Explore Anime</h2>
+			<section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+				{data.map((item: AnimeProp, index: number) => (
+					<AnimeCard anime={item} index={index} key={item.id} />
+				))}
+			</section>
+		</main>
+	);
 }
